cb7fe573fae12dc9537c3874f2af15833a89744a
mrsekut <k.cloudspider@gmail.com>
2024-12-15 18:26:29 +0900

    get windows list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core_foundation::array::CFArray;
-use core_foundation::base::{CFTypeRef, TCFType};
+use core_foundation::base::{CFTypeRef, TCFType, TCFTypeRef};
 use core_foundation::dictionary::CFDictionary;
 use core_foundation::string::CFString;
 use core_graphics::window::{kCGWindowListOptionOnScreenOnly, CGWindowListCopyWindowInfo};
@@ -8,36 +8,31 @@ fn main() {
     unsafe {
         // 画面上のすべてのウィンドウ情報を取得
         let window_list = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, 0);
-
-        if window_list.is_null() {
-            println!("ウィンドウリストを取得できませんでした。");
-            return;
-        }
-
-        // CFArray型にキャスト
         let windows: CFArray<CFDictionary<CFString, CFTypeRef>> =
             CFArray::wrap_under_get_rule(window_list);
 
-        println!("ウィンドウの数: {}", windows.len());
-
-        for (i, window) in windows.iter().enumerate() {
-            // CFDictionaryを操作する
+        windows.iter().for_each(|window| {
             if let Some(name) = get_string_from_dict(&window, "kCGWindowName") {
-                println!("ウィンドウ {} の名前: {}", i, name);
+                println!("ウィンドウの名前: {}", name);
             }
-        }
+
+            if let Some(owner) = get_string_from_dict(&window, "kCGWindowOwnerName") {
+                println!("ウィンドウのオーナー: {}", owner);
+            }
+        });
     }
 }
 
-// /// CFDictionaryから指定したキーの値を取得し、RustのStringに変換
-// fn get_string_from_dict(dict: &CFDictionary<CFString, CFTypeRef>, key: &str) -> Option<String> {
-//     let cf_key = CFString::from_static_string(key);
-//     let value = dict.find(&cf_key)?;
+fn get_string_from_dict(dict: &CFDictionary<CFString, CFTypeRef>, key: &str) -> Option<String> {
+    let cf_key = CFString::new(key);
+    let value_ref = dict.find(&cf_key)?;
+    let value_ptr = value_ref.as_void_ptr();
 
-//     // CFTypeRefをCFStringにキャスト
-//     if value.is_null() {
-//         return None;
-//     }
-//     let cf_string: CFString = unsafe { CFString::wrap_under_get_rule(value as *const _) };
-//     Some(cf_string.to_string())
-// }
+    if value_ptr.is_null() {
+        return None;
+    }
+
+    let cf_string: CFString = unsafe { CFString::wrap_under_get_rule(value_ptr as *const _) };
+
+    Some(cf_string.to_string())
+}
